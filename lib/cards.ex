e11813ca8431c8cac4588484af2f68e88eaf1216
Lucas <LucasAmaral42>
2021-08-09 23:41:08 -0300

    section 2, elixir's amazing pattern matching

--- lib/cards.ex
+++ lib/cards.ex
@@ -3,7 +3,7 @@ defmodule Cards do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    cards = for suit <- suits, value <- values do
+    for suit <- suits, value <- values do
       "#{value} of #{suit}"
     end
   end
@@ -18,5 +18,23 @@ defmodule Cards do
 
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary}  -> :erlang.binary_to_term(binary)
+      {:error, _msg} -> "File not exists"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
